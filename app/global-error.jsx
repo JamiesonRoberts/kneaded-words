0f644ef4b3f5b0ac263b08d8e5046e86bc576c50
Jamieson Roberts <hello@jamiesonroberts.com>
2024-01-14 14:15:31 -0500

    Working on base

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,19 +1,19 @@
-"use client";
+'use client'
 
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
-import { useEffect } from "react";
+import * as Sentry from '@sentry/nextjs'
+import Error from 'next/error'
+import { useEffect } from 'react'
 
 export default function GlobalError({ error }) {
   useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    Sentry.captureException(error)
+  }, [error])
 
   return (
     <html>
       <body>
-        <Error />
+        <Error statusCode={error.statusCode} />
       </body>
     </html>
-  );
+  )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,30 @@
+import CloudflareWebAnalyticsProvider from 'next-cloudflare-web-analytics'
+
+export const viewport = {
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
+  themeColor: '#FFFFFF',
+  colorScheme: 'light',
+}
+
+export const metadata = {
+  metadataBase: new URL('https://kneadedwords.ca/'),
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/icon.svg', type: 'image/svg+xml', sizes: 'any' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+}
+
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='en-CA' className={``}>
+      <CloudflareWebAnalyticsProvider token={'XXXX'} />
       <body>{children}</body>
     </html>
   )
